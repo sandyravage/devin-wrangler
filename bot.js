6f20c0d85d15ea0dec339a214eec85e4b241a613
benis lord <suckit@trebek.com>
2022-08-07 12:23:28 -0400

    do the thing

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const pixelmatch = require('pixelmatch');
 const axios = require('axios').default;
+const sharp = require('sharp');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
 
@@ -12,12 +13,15 @@ client.once('ready', () => {
 client.login(process.env.DISCORD_TOKEN);
 
 const refData = fs.readFileSync('./recursion.png');
+const refBuffer = sharp(refData).resize(684, 716).toBuffer();
 
 client.on("messageCreate", async (msg) => {
     if(msg.author.bot || msg.attachments.size == 0) return;
     const response = await axios.get(msg.attachments.first().url, { responseType: 'arraybuffer' });
     const currentData = Buffer.from(response.data);
-    const diff = pixelmatch(refData, currentData, null, 684, 716);
+    const currentBuffer = sharp(currentData).resize(684, 716).toBuffer();
+
+    const diff = pixelmatch(refBuffer, currentData, null, 684, 716);
 
     msg.channel.send("got diff: " + diff);
     //client.channels.cache.get(msg.channel.id).send(`also deleted ur message partner. <@${msg.author.id}> hee hee`);
