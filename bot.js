845ba87b650c02e6a6aa4cd79cdcfca8ce92a415
benis lord <suckit@trebek.com>
2022-08-07 11:48:07 -0400

    so sloppy baby

--- bot.js
+++ bot.js
@@ -16,8 +16,7 @@ const refData = fs.readFileSync('./recursion.png', 'utf8');
 let array = [];
 
 client.on("messageCreate", async (msg) => {
-    console.log("got embeds length: " + msg.attachments.length);
-    if(msg.author.bot || msg.attachments.length == 0) return;
+    if(msg.author.bot || msg.attachments.size == 0) return;
     const response = await fetch(msg.attachments.first().url);
     const arrayBuffer = await response.arrayBuffer();
     const currentData = Buffer.from(arrayBuffer);
