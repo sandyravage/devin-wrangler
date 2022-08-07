25923a059d2e764628a1099c981c2cabc7e5b7a4
benis lord <suckit@trebek.com>
2022-08-07 11:47:31 -0400

    making more sense

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const fetch = require('fetch');
 const pixelmatch = require('pixelmatch');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]})
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]})
 
 client.once('ready', () => {
     console.log('Bot Ready!');
@@ -16,13 +16,13 @@ const refData = fs.readFileSync('./recursion.png', 'utf8');
 let array = [];
 
 client.on("messageCreate", async (msg) => {
-    console.log("got embeds length: " + msg.embeds.length);
-    if(msg.author.bot || msg.embeds.length == 0) return;
-    const response = await fetch(msg.embeds.first().url);
+    console.log("got embeds length: " + msg.attachments.length);
+    if(msg.author.bot || msg.attachments.length == 0) return;
+    const response = await fetch(msg.attachments.first().url);
     const arrayBuffer = await response.arrayBuffer();
     const currentData = Buffer.from(arrayBuffer);
     const diff = pixelmatch(refData, currentData, null, 684, 716);
 
-    client.channels.cache.get(msg.channel.id).send("got diff: " + diff);
+    msg.channel.send("got diff: " + diff);
     //client.channels.cache.get(msg.channel.id).send(`also deleted ur message partner. <@${msg.author.id}> hee hee`);
 });
