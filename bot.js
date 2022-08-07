0ff749c02bfe73a87cfa20cacc7e46243d6a0b8a
benis lord <suckit@trebek.com>
2022-08-07 12:56:34 -0400

    do the thing

--- bot.js
+++ bot.js
@@ -15,21 +15,19 @@ client.login(process.env.DISCORD_TOKEN);
 
 const refData = fs.readFileSync('./recursion.png');
 var refBuffer;
- sharp(refData).toFormat('png').resize(684, 716).toBuffer().then(data => refBuffer = data);
-
+sharp(refData).toFormat('png').resize(684, 716).toBuffer().then(data => refBuffer = data);
 const refImg = PNG.sync.read(refData);
-console.log("got ref image width: " + refImg.width);
-console.log("got ref image height: " + refImg.height);
+
 client.on("messageCreate", async (msg) => {
     if(msg.author.bot || msg.attachments.size == 0) return;
     const response = await axios.get(msg.attachments.first().url, { responseType: 'arraybuffer' });
     const currentData = Buffer.from(response.data);
     const currentBuffer = await sharp(currentData).toFormat('png').resize(684, 716).toBuffer();
     const currentImg = PNG.sync.read(currentBuffer);
-    console.log("got current image width: " + currentImg.width);
-    console.log("got current image height: " + currentImg.height);
     const diff = pixelmatch(refImg.data, currentImg.data, null, 684, 716);
-
-    msg.channel.send("got diff: " + diff);
-    //client.channels.cache.get(msg.channel.id).send(`also deleted ur message partner. <@${msg.author.id}> hee hee`);
+    if(diff < 60000) {
+        msg.delete();
+        msg.channel.send(`Uh oh Partners! Looks like <@${msg.author.id}> tried to post cringe! I've done deleted that pesky message for now, `
+            + "but lets try and keep a better lookout in the future!");
+    }
 });
