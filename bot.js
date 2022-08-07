a4fc4ee5da3adf5573f32b0fdc66bfc4ea585ccc
benis lord <suckit@trebek.com>
2022-08-07 10:36:42 -0400

    do more things

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]})
 
 client.once('ready', () => {
     console.log('Bot Ready!');
