7d48114fade54c9455a197b212b148becdd465b7
benis lord <suckit@trebek.com>
2022-08-07 12:27:37 -0400

    do the thing

--- bot.js
+++ bot.js
@@ -13,15 +13,16 @@ client.once('ready', () => {
 client.login(process.env.DISCORD_TOKEN);
 
 const refData = fs.readFileSync('./recursion.png');
-const refBuffer = sharp(refData).resize(684, 716).toBuffer();
+var refBuffer;
+sharp(refData).resize(684, 716).toBuffer().then(data => refBuffer = data);
 
 client.on("messageCreate", async (msg) => {
     if(msg.author.bot || msg.attachments.size == 0) return;
     const response = await axios.get(msg.attachments.first().url, { responseType: 'arraybuffer' });
     const currentData = Buffer.from(response.data);
-    const currentBuffer = sharp(currentData).resize(684, 716).toBuffer();
+    const currentBuffer = await sharp(currentData).resize(684, 716).toBuffer();
 
-    const diff = pixelmatch(refBuffer, currentData, null, 684, 716);
+    const diff = pixelmatch(refBuffer, currentBuffer, null, 684, 716);
 
     msg.channel.send("got diff: " + diff);
     //client.channels.cache.get(msg.channel.id).send(`also deleted ur message partner. <@${msg.author.id}> hee hee`);
